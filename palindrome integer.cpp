bc801b9ba12e3ccf33b98730f4caf9e1d75d51f1
aneripatel98 <aneripatel98@gmail.com>
2018-07-11 05:38:25 -0600

    Add files via upload

--- palindrome integer.cpp
+++ palindrome integer.cpp
@@ -1,18 +1,27 @@
 int Solution::isPalindrome(int A) {
-    int size=0,i=0,b=A,c=A,flag=0;
-    if(b<0) return flag;
-    while(b){
-        b/=10;
-        size++;
-    }
+    // int size=0,i=0,b=A,c=A,flag=0;
+    // if(b<0) return flag;
+    // while(b){
+    //     b/=10;
+    //     size++;
+    // }
     
-    if(size==1||size==0) return flag++;
+    // if(size==1||size==0) return flag++;
     
-    for(i=0;i<size/2;i++){
-        if((c/(10^(size-i-1)))%10 != (c/(10^i))%10){
-            flag=0;
-            break;
+    // for(i=0;i<size/2;i++){
+    //     if((c/(10^(size-i-1)))%10 != (c/(10^i))%10){
+    //         flag=0;
+    //         break;
+    //     }
+    // }
+    string b = to_string(A);
+    // cout<<b;
+    int i=0,j=b.size()-1;
+    while(i<j){
+        if(b[i]==b[j]){
+            i++;j--;
         }
+        else return 0;
     }
-    return flag;
+    return 1;
 }
